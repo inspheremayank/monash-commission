325408595a20fd4ed62a18531d55c0960e2aaf43
insphere_mayank <insphere.mayank@gmail.com>
2018-04-07 11:55:26 +0530

    updates

--- includes/_footer.php
+++ includes/_footer.php
@@ -2,96 +2,21 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <div class="footer__main">
-                    <div class="row">
-                        <div class="col-12 col-md-3">
-                            <div class="footer__main__content">
-                                <div class="footer__main__content--logo">
-                                    <a href="javascript:;">
-                                        <img src="/static/images/gv-health-logo.svg" class="img-fluid" alt="logo" />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-6 col-md-2 col-xxs-12">
-                            <div class="footer__main__content">
-                                <div class="footer__main__content--heading">Category One</div>
-                                <ul class="footer__main__content--menu">
-                                    <li>
-                                        <a href="javascript:;">Menu One</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">Menu One</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">Menu One</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">Menu One</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-6 col-md-2 col-xxs-12">
-                            <div class="footer__main__content">
-                                <div class="footer__main__content--heading">Category Two</div>
-                                <ul class="footer__main__content--menu">
-                                    <li>
-                                        <a href="javascript:;">Menu One</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">Menu One</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">Menu One</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">Menu One</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-6 col-md-2 col-xxs-12">
-                            <div class="footer__main__content">
-                                <div class="footer__main__content--heading">Category Three</div>
-                                <ul class="footer__main__content--menu">
-                                    <li>
-                                        <a href="javascript:;">Menu One</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">Menu One</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">Menu One</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">Menu One</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-6 col-md-3 col-xxs-12">
-                            <div class="footer__main__content">
-                                <div class="footer__main__content--heading">Follow us</div>
-                                <div class="footer__main__content--social-media">
-                                    <a href="javascript:;" class="fa fa-envelope"></a>
-                                    <a href="javascript:;" class="fa fa-twitter"></a>
-                                    <a href="javascript:;" class="fa fa-linkedin"></a>
-                                    <a href="javascript:;" class="fa fa-vimeo"></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div class="footer-logo">
+                    <a href="javascript:;">
+                        <img class="img-fluid" src="static/images/logo-white.svg" alt="image" />
+                    </a>
                 </div>
-                <div class="footer__bottom">
-                    <div class="footer__bottom--ls">&copy; 2018 Goulburn Valley Health</div>
-                    <div class="footer__bottom--rs">
-                        <div class="footer__bottom--navigation">
-                            <a href="javascript:;">Disclaimer</a>
-                            <a href="javascript:;">Copyright</a>
-                            <a href="javascript:;">Privacy</a>
-                            <a href="javascript:;">Accessibility</a>
-                        </div>
+                <div class="footer-bottom">
+                    <div class="footer-bottom__navigation">
+                        <a href="javascript:;">About</a>
+                        <a href="javascript:;">Terms of Use</a>
+                        <a href="javascript:;">Contact</a>
+                    </div>
+                    <div class="footer-bottom__social">
+                        <a class="fa fa-linkedin-square" href="javascript:;"></a>
+                        <a class="fa fa-twitter" href="javascript:;"></a>
+                        <a class="fa fa-youtube-square" href="javascript:;"></a>
                     </div>
                 </div>
             </div>
--- index.php
+++ index.php
@@ -7,34 +7,7 @@ include("includes/_global-Head.php");
     include("includes/_header.php");
 ?>
 <div class="clearfix"></div>
-<div class="getForm__wrapper">
-    <div class="container-fluid">
-        <div class="row justify-content-between align-items-center">
-            <div class="col-12 col-md-6 px-0 flex-last">
-                <aside class="getForm__wrapper-image">
-                    <img src="static/images/preview.png" class="img-fluid" alt="Laptop image" />
-                </aside>
-            </div>
-            <div class="col-12 col-md-6 px-0 flex-first">
-                <aside class="getForm__wrapper-form containerFluid-content containerFluid-content-margin100">
-                    <h1 class="title">Subscribe</h1>
-                    <p>Stay up to date with all the Goulbourn Valley Health updates and news.</p>
-                    <div class="form-fields">
-                        <div class="form-group">
-                            <label for="username" class="sr-only">Name</label>
-                            <input type="text" class="form-control" id="username" name="username" placeholder="Name" />
-                        </div>
-                        <div class="form-group">
-                            <label for="email" class="sr-only">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Email address" />
-                        </div>
-                        <button onclick="window.location.href='home.php'" class="button upper button--radius button--blue">let's go</button>
-                    </div>
-                </aside>
-            </div>
-        </div>
-    </div>
-</div>
+
 <div class="clearfix"></div>
 <?php
     include("includes/_footer.php");
