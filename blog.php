ffd8f80001e997960363fd117853ea50564897ee
insphere_mayank <insphere.mayank@gmail.com>
2018-04-09 12:06:29 +0530

    blog page markup added...

---
+++ blog.php
@@ -0,0 +1,99 @@
+<!-- Begin global header section -->
+<?php
+$pageTitle = 'Monash Commission';
+include("includes/_global-Head.php");
+?>
+<?php
+include("includes/_header.php");
+?>
+<div class="clearfix"></div>
+<div class="page__body">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="page__body--wrapper">
+                    <div class="blog__wrapper image-covered" style="background-image: url('static/images/image1.jpg')">
+                        <div class="blog__wrapper__userInfo">
+                            <div class="blog__wrapper__userInfo--title">Christine Lagarde</div>
+                            <div class="blog__wrapper__userInfo--description">Passionate about the intersection of public policy and economics.</div>
+                            <div class="blog__wrapper__userInfo--social">
+                                <a href="javascript:;" class="fa fa-linkedin-square"></a>
+                                <a href="javascript:;" class="fa fa-twitter"></a>
+                            </div>
+                        </div>
+                        <div class="blog__wrapper__userDetail">
+                            <div class="blog__wrapper__userDetail__profile">
+                                <div class="blog__wrapper__userDetail__profile--image image-covered" style="background-image: url('static/images/icons/avatar.png')"></div>
+                                <div class="blog__wrapper__userDetail__profile--content">
+                                    <div class="blog__wrapper__userDetail__profile--content--title">Christine Lagarde</div>
+                                    <div class="blog__wrapper__userDetail__profile--content--des">
+                                        <span>Economist</span><span>Leader</span><span>Thinker</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="blog__wrapper__userDetail--content">
+                                <p>Christine Madeleine Odette Lagarde  is a French lawyer and politician who has been the Managing Director (MD) of the International Monetary Fund (IMF) since 5 July 2011.</p>
+                                <p>Previously, she held various ministerial posts in the French government: she was Minister of Economic Affairs, Finance and Employment, Minister of Agriculture and Fishing and Minister of Trade in the government of Dominique de Villepin. Lagarde was the first woman to become finance minister of a G8 economy and is the first woman to head the IMF. A noted anti-trust and labour lawyer, Lagarde was the first female chair of the international law firm Baker & McKenzie.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-wrap">
+                    <div class="row">
+                        <div class="card__view col-12 card__view-full">
+                            <div class="">
+                                <a href="javascript:;" class="card__view--items">
+                                    <figure class="embed-responsive embed-responsive-16by9">
+                                        <img src="static/images/image1.jpg" alt="image" class="embed-responsive-item" />
+                                        <div class="video-play"></div>
+                                    </figure>
+                                    <div class="optionSet">
+                                        <ul>
+                                            <li>Hide <span class="fa fa-eye-slash"></span></li>
+                                            <li>Pin <span class="fa fa-map-marker"></span></li>
+                                            <li>Edit <span class="fa fa-cog"></span></li>
+                                        </ul>
+                                    </div>
+                                    <div class="card__view--content">
+                                        <div class="content__section">
+                                            <div class="social-icons"></div>
+                                            <div class="content__section-category upper">Finance</div>
+                                            <div class="content__section__head">
+                                                <div class="content__section__head--heading">A lose-lose trade war looms between America and China, and everyone suffers</div>
+                                                <div class="content__section__head--image">
+                                                    <figure class="image-covered" style="background-image: url('static/images/image1.jpg')"></figure>
+                                                    <div class="video-play"></div>
+                                                </div>
+                                            </div>
+                                            <div class="content__section-description text__dotdot">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                                            <div class="content__section-userInfo"> 
+                                                <div class="content__section-userInfo-image image-covered" style="background-image: url('static/images/test-image.jpg')"></div> 
+                                                <div class="content__section-userInfo-content"> 
+                                                    <span>Rebekah Brown</span> 
+                                                    <span class="designation">Director of Monash Sustainable Development Institute</span> 
+                                                </div> 
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="clearfix"></div>
+<?php
+include("includes/_footer.php");
+?>
+<div class="clearfix"></div>
+<!-- Begin Script section -->
+<?php
+include("includes/_scripts.php");
+?>
+<!-- Begin Script section -->
+
+</body>
+</html>
