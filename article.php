3f0100d2198ef21b0641d1a0cc5577022d0a05ae
insphere_mayank <insphere.mayank@gmail.com>
2018-04-09 11:13:47 +0530

    article page markup completed..

--- article.php
+++ article.php
@@ -12,45 +12,51 @@ include("includes/_global-Head.php");
         <div class="row">
             <div class="col-12">
                 <div class="page__body--wrapper">
-                    <div class="card-wrap">
-                        <div class="row">
-                            <div class="card__view col-12 card__view-full">
-                                <div class="">
-                                    <a href="javascript:;" class="card__view--items">
-                                        <figure class="embed-responsive embed-responsive-16by9">
-                                            <img src="static/images/image1.jpg" alt="image" class="embed-responsive-item" />
-                                            <div class="video-play"></div>
-                                        </figure>
-                                        <div class="optionSet">
-                                            <ul>
-                                                <li>Hide <span class="fa fa-eye-slash"></span></li>
-                                                <li>Pin <span class="fa fa-map-marker"></span></li>
-                                                <li>Edit <span class="fa fa-cog"></span></li>
-                                            </ul>
-                                        </div>
-                                        <div class="card__view--content">
-                                            <div class="content__section">
-                                                <div class="social-icons"></div>
-                                                <div class="content__section-category upper">Finance</div>
-                                                <div class="content__section__head">
-                                                    <div class="content__section__head--heading">A lose-lose trade war looms between America and China, and everyone suffers</div>
-                                                    <div class="content__section__head--image">
-                                                        <figure class="image-covered" style="background-image: url('static/images/image1.jpg')"></figure>
-                                                        <div class="video-play"></div>
-                                                    </div>
-                                                </div>
-                                                <div class="content__section-description text__dotdot">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
-                                                <div class="content__section-userInfo"> 
-                                                    <div class="content__section-userInfo-image image-covered" style="background-image: url('static/images/test-image.jpg')"></div> 
-                                                    <div class="content__section-userInfo-content"> 
-                                                        <span>Rebekah Brown</span> 
-                                                        <span class="designation">Director of Monash Sustainable Development Institute</span> 
-                                                    </div> 
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </a>
+                    <div class="article__wrapper">
+                        <div class="article__main">
+                            <div class="article__main__blog-detail">
+                                <div class="article__main__blog-detail--edit">Edit Article</div>
+                                <div class="article__main__blog-detail--title">How the West got China wrong</div>
+                                <div class="article__main__blog-detail--description">It bet that China would head towards democracy and the market economy. The gamble has failed.</div>
+                                <div class="article__main__blog-detail--category">China</div>
+                            </div>
+                            <div class="article__slide">
+                                <div class="mainSlide slider">
+                                    <div class="item">
+                                        <img src="static/images/image1.jpg" class="img-fluid" alt="image" />
+                                        <div class="image-caption">Caption will come here</div>
+                                    </div>
+                                    <div class="item">
+                                        <img src="static/images/image1.jpg" class="img-fluid" alt="image" />
+                                    </div>
+                                    <div class="item">
+                                        <img src="static/images/image1.jpg" class="img-fluid" alt="image" />
+                                        <a class="playFancyVideo" data-fancybox href="https://youtu.be/PaYbvJNiWyM"></a>
+                                    </div>
                                 </div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="article__main__info">
+                                <div class="article__main__info__user">
+                                    <div class="article__main__info__user--content">
+                                        <a href="javasrcipt:;">
+                                            <figure class="image-covered rounded-circle" style="background-image:url('static/images/icons/avatar.png');"></figure>
+                                            <div class="title">Title here</div>
+                                            <div class="designation">designation here</div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="article__main__info__social">
+                                    <a href="javasrcipt:;"><span class="fa fa-facebook"></span></a>
+                                    <a href="javasrcipt:;"><span class="fa fa-twitte"></span></a>
+                                    <a href="javasrcipt:;"><span class="fa fa-linkedin"></span></a>
+                                    <a href="javasrcipt:;"><span class="fa fa-envelope"></span></a>
+                                    <a href="javasrcipt:;"><span class="fa fa-print"></span></a>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="article__main-content">
+                                content will be here...
                             </div>
                         </div>
                     </div>
